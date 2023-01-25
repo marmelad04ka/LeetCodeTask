1c07b75ae964ac560be31e97aa83790be8dd207c
marmelad04ka <47591076+marmelad04ka@users.noreply.github.com>
2023-01-25 13:17:15 +0300

    12

---
+++ Twelve.java
@@ -0,0 +1,42 @@
+public class Twelve {
+    //584. Find Customer Referee
+    //https://leetcode.com/problems/find-customer-referee/?envType=study-plan&id=sql-i
+//    Select name
+//    From Customer
+//    Where referee_id != 2 OR referee_id IS NULL;
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+}
