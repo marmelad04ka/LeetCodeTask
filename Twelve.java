13e0518275c90387f0eee4a43f3e6093786c2394
marmelad04ka <47591076+marmelad04ka@users.noreply.github.com>
2023-01-25 13:45:16 +0300

    12

--- Twelve.java
+++ Twelve.java
@@ -5,8 +5,11 @@ public class Twelve {
 //    From Customer
 //    Where referee_id != 2 OR referee_id IS NULL;
 
-    //
-    //
+    //183. Customers Who Never Order
+    //https://leetcode.com/problems/customers-who-never-order/description/?envType=study-plan&id=sql-i
+//    Select name AS 'Customers'
+//    From Customers
+//    Where id NOT IN (Select customerId from Orders);
 
 
     //
