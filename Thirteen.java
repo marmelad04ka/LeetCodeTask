e89102620b3afc8a349afd1b4628a0e30aba6f20
marmelad04ka <47591076+marmelad04ka@users.noreply.github.com>
2023-01-27 14:10:35 +0300

    13

--- Thirteen.java
+++ Thirteen.java
@@ -5,21 +5,32 @@ public class Thirteen {
 //    from Patients
 //    Where conditions like 'DIAB1%' or conditions like '% DIAB1%';
 
-    //
-    //
+    //511. Game Play Analysis I
+    //https://leetcode.com/problems/game-play-analysis-i/description/
+//    select player_id, min(event_date) as first_login
+//    from Activity
+//    group by player_id;
 
+    //1148. Article Views I
+    //https://leetcode.com/problems/article-views-i/description/
+//    select distinct author_id as id
+//    from Views
+//    where author_id = viewer_id
+//    order by author_id;
 
-    //
-    //
+    //620. Not Boring Movies
+    //https://leetcode.com/problems/not-boring-movies/description/
+//    select id, movie, description, rating
+//    from Cinema
+//    where id%2 = 1 and description not like 'boring'
+//    order by rating desc;
 
-
-    //
-    //
-
-
-    //
-    //
-
+    //596. Classes More Than 5 Students
+    //https://leetcode.com/problems/classes-more-than-5-students/description/
+//    select class
+//    from courses
+//    group by class
+//    having count(student) >= 5;
 
     //
     //
