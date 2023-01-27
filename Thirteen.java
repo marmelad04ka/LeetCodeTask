d21dfcf8f6d34dabcd04226b2a29d59e13f8fbe9
marmelad04ka <47591076+marmelad04ka@users.noreply.github.com>
2023-01-27 12:24:06 +0300

    13

---
+++ Thirteen.java
@@ -0,0 +1,42 @@
+public class Thirteen {
+    //1527. Patients With a Condition
+    //https://leetcode.com/problems/patients-with-a-condition/description/?envType=study-plan&id=sql-i
+//    select patient_id, patient_name, conditions
+//    from Patients
+//    Where conditions like 'DIAB1%' or conditions like '% DIAB1%';
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+
+
+    //
+    //
+}
