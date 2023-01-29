bf9ef01ba6110950dbb0d60dd0dffb6f70d78c31
marmelad04ka <47591076+marmelad04ka@users.noreply.github.com>
2023-01-29 11:17:32 +0300

    16

---
+++ Sixteen.java
@@ -0,0 +1,15 @@
+public class Sixteen {
+    //175. Combine Two Tables
+    //https://leetcode.com/problems/combine-two-tables/description/?envType=study-plan&id=sql-i
+//    Select firstName, lastName, city, state
+//    from Person left join Address
+//    using (personId);
+
+    //1148. Article Views I
+    //https://leetcode.com/problems/article-views-i/description/?envType=study-plan&id=sql-i
+//    select distinct author_id as id
+//    from Views
+//    where author_id = viewer_id
+//    order by author_id;
+
+}
